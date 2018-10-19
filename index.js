6b90c608f842afc53ad0e40caa609994b65afa88
LandrumTrev <landrum.trevillian@gmail.com>
2018-10-19 17:27:31 -0400

    files structure

---
+++ index.js
@@ -0,0 +1,23 @@
+// ====================================================
+// WORD GUESS CLI node.js app
+// ©2018 Richard Trevillian
+// University of Richmond (Virginia)
+// Full Stack Developer Bootcamp (July 2018)
+// ====================================================
+
+// index.js
+// point-of-entry file to the application
+
+// main logic for game play
+// randomly selects a word, using the Word constructor to store the word
+// prompts user to guess the next letter
+// keeps track of user guesses remaining
+
+// index.js requires word.js, inquirer
+// word.js requires letter.js
+// letter.js (no requires)
+
+var inquirer = require("inquirer");
+
+var Word = require("./word.js");
+
---
+++ letter.js
@@ -0,0 +1,30 @@
+// ====================================================
+// WORD GUESS CLI node.js app
+// ©2018 Richard Trevillian
+// University of Richmond (Virginia)
+// Full Stack Developer Bootcamp (July 2018)
+// ====================================================
+
+// letter.js
+// contains the Letter constructor, used by word.js (Word constructor)
+
+// displays either a blank (unguessed letter) or displays a guessed letter
+// Letter constructor defines:
+
+// var hidden = "a"; string value to store the value of the letter
+// var guessed = FALSE; boolean value for whether the letter was guessed or not
+
+// function characterDisplay() {} 
+// that returns either letter 
+// (if guessed === TRUE) { return letter };
+// or a blank (underscore) 
+// (if guessed === FALSE) { return "_"};
+
+// function checker(guess) {} 
+// that takes a user guess letter and checks to see 
+// if(guess === hidden)
+// IF TRUE, guessed = TRUE;
+// IF FALSE, decrement guessesRemaining--;
+
+
+
